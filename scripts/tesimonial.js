b1acf6a2197e4dfbea30effdd36e61a8bc8d2934
dheerajdlalwani <lalwanidheeraj1234@gmail.com>
2021-05-08 12:17:10 +0530

    complete the testimonial slider

--- scripts/tesimonial.js
+++
@@ -1,7 +0,0 @@
-let content= document.querySelector('.testimonials-message')
-let templateTestimonialCards= dataset=>{
-    let html=``
-    dataset.forEach(data=> {
-        html=
-    })
-}
---
+++ scripts/testimonial.js
@@ -0,0 +1,106 @@
+let testimonialHTML = [];
+let currentTestimonial = 0;
+let timer = null;
+
+const getTestimonialData = async () => 
+{
+    const response = await fetch("../json/testimonial.json");
+    const dataset = await response.json();
+    return dataset;
+}
+
+
+
+function slideShow()
+{
+    timer = setInterval (e => {
+        let target = document.querySelector('#message');
+        target.innerHTML = "";
+        target.innerHTML = testimonialHTML[currentTestimonial];
+        console.log(target.innerHTML, currentTestimonial);
+        if(currentTestimonial == testimonialHTML.length - 1)
+        {
+            currentTestimonial = 0;
+        }
+        else
+        {
+            currentTestimonial += 1;
+        }
+    }, 3000)
+}
+
+
+getTestimonialData()
+.then(
+        dataset => 
+        {
+            dataset.forEach(testimonial => {
+                testimonialHTML.push(`${testimonial}`)
+            });
+            
+        },
+        document.querySelector('#message').innerHTML = testimonialHTML[0],
+        slideShow()
+    ) 
+.catch(
+    err => 
+    console.log(err)
+);
+
+    
+
+
+let btnLeft = document.querySelector('.testimonial-arrow-left');
+let btnRight = document.querySelector('.testimonial-arrow-right');
+
+
+function goLeft()
+{
+    if(currentTestimonial == 0)
+    {
+        currentTestimonial = 4;
+    }
+    else
+    {
+        currentTestimonial -= 1;
+    }
+    let target = document.querySelector('#message');
+    target.innerHTML = "";
+    target.innerHTML = testimonialHTML[currentTestimonial];
+
+}
+
+function goRight()
+{
+    if(currentTestimonial == 4)
+    {
+        currentTestimonial = 0;
+    }
+    else
+    {
+        currentTestimonial += 1;
+    }
+    let target = document.querySelector('#message');
+    target.innerHTML = "";
+    target.innerHTML = testimonialHTML[currentTestimonial];
+
+}
+
+
+
+console.log(btnLeft);
+console.log(btnRight);
+
+btnLeft.addEventListener('click', e => {
+    console.log("Go Left");
+    clearInterval(timer);
+    goLeft();
+})
+
+
+
+btnRight.addEventListener('click', e => {
+    console.log("Go Right");
+    clearInterval(timer);
+    goRight();
+})
