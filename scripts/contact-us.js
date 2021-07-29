e832f694328344d7142cddedbdb6f8c4fc25e08f
dheerajdlalwani <lalwanidheeraj1234@gmail.com>
2021-07-29 23:26:20 +0530

    add menu and hamburger menu

--- scripts/contact-us.js
+++ scripts/contact-us.js
@@ -10,3 +10,17 @@ function closeContactForm()
     document.querySelector(".contact-us").classList.toggle("hidden")
 }
 
+
+var today = new Date()
+document.getElementById('date-submitted').value=today;
+console.log(today)
+
+
+const scriptURL = 'https://script.google.com/macros/s/AKfycbxgljTOpbAuIsgPRh-FIeEpUosOoe8BK2BPf21DW623_5W7iwCvCXZ-Bt00qlpFbfiW/exec'
+const form = document.forms['submit-to-google-sheet']
+form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+      .then(response => console.log('Success!', response))
+      .catch(error => console.error('Error!', error.message))
+  })
