4d3651e642b1fcb08a33c98f58b64f9c088a306e
dheerajdlalwani <lalwanidheeraj1234@gmail.com>
2021-08-07 12:37:49 +0530

    contact form fixed.

--- scripts/contact-us.js
+++ scripts/contact-us.js
@@ -12,10 +12,24 @@ closeBtn.addEventListener("click", closeContactForm)
 openBtn.addEventListener("click", closeContactForm)
 openBtnMob.addEventListener("click", closeContactForm)
 openBtnGIT.addEventListener("click", closeContactForm)
-openBtnEnrolNowSCDesktop.addEventListener("click", closeContactForm)
-openBtnEnrolNowSCMobile.addEventListener("click", closeContactForm)
-openBtnEnrolNowRC.addEventListener("click", closeContactForm)
-openBtnEnrolNowCC.addEventListener("click", closeContactForm)
+
+
+if (openBtnEnrolNowSCDesktop) {
+    openBtnEnrolNowSCDesktop.addEventListener("click", closeContactForm)
+}
+
+if (openBtnEnrolNowSCMobile) {
+    openBtnEnrolNowSCMobile.addEventListener("click", closeContactForm)
+}
+
+if (openBtnEnrolNowRC) {
+    openBtnEnrolNowRC.addEventListener("click", closeContactForm)
+}
+
+if (openBtnEnrolNowCC) {
+    openBtnEnrolNowCC.addEventListener("click", closeContactForm)
+}
+
 
 function closeContactForm()
 {
@@ -32,6 +46,8 @@ console.log(today)
 
 const scriptURL = 'https://script.google.com/macros/s/AKfycbxgljTOpbAuIsgPRh-FIeEpUosOoe8BK2BPf21DW623_5W7iwCvCXZ-Bt00qlpFbfiW/exec'
 const form = document.forms['submit-to-google-sheet']
+console.log(form)
+
 form.addEventListener('submit', e => {
     e.preventDefault()
     fetch(scriptURL, { method: 'POST', body: new FormData(form)})
