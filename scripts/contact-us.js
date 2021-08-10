889e693668ae68d2331e05498087504c0c49f101
dheerajdlalwani <lalwanidheeraj1234@gmail.com>
2021-08-11 01:05:51 +0530

    fixed enroll-now buttons

--- scripts/contact-us.js
+++ scripts/contact-us.js
@@ -1,52 +1,37 @@
 const closeBtn = document.querySelector(".close-contact-us");
-const openBtn = document.querySelector(".open-contact-us");
+const openBtn = document.querySelector(".open-contact-us-nav");
 const openBtnMob = document.querySelector(".open-contact-us-mob")
-const openBtnGIT= document.querySelector(".open-contact-us-GIT")
-const openBtnEnrolNowSCDesktop= document.querySelector(".open-contact-us-GIT-SCD")
-const openBtnEnrolNowSCMobile= document.querySelector(".open-contact-us-GIT-SCM")
-const openBtnEnrolNowRC= document.querySelector(".open-contact-us-GIT-RC")
-const openBtnEnrolNowCC= document.querySelector(".open-contact-us-GIT-CC")
+const openContactUsBtns = document.getElementsByClassName("open-contact-us")
+console.log(openContactUsBtns)
 
 
+for (let btn of openContactUsBtns) {
+    btn.addEventListener("click", toggleContactForm)
+}
 closeBtn.addEventListener("click", closeContactForm)
 openBtn.addEventListener("click", closeContactForm)
 openBtnMob.addEventListener("click", closeContactForm)
-openBtnGIT.addEventListener("click", closeContactForm)
 
 
-if (openBtnEnrolNowSCDesktop) {
-    openBtnEnrolNowSCDesktop.addEventListener("click", closeContactForm)
-}
-
-if (openBtnEnrolNowSCMobile) {
-    openBtnEnrolNowSCMobile.addEventListener("click", closeContactForm)
-}
-
-if (openBtnEnrolNowRC) {
-    openBtnEnrolNowRC.addEventListener("click", closeContactForm)
-}
-
-if (openBtnEnrolNowCC) {
-    openBtnEnrolNowCC.addEventListener("click", closeContactForm)
-}
-
-
-function closeContactForm()
+function toggleContactForm()
 {
     document.querySelector(".overlay").classList.toggle("hidden")
     document.querySelector(".contact-us").classList.toggle("hidden")
-    console.log("SoMeThInG")
+}
+
+function closeContactForm()
+{
+    toggleContactForm()
     expandMenu()
 }
 
 var today = new Date()
 document.getElementById('date-submitted').value=today;
-console.log(today)
 
 
 const scriptURL = 'https://script.google.com/macros/s/AKfycbxgljTOpbAuIsgPRh-FIeEpUosOoe8BK2BPf21DW623_5W7iwCvCXZ-Bt00qlpFbfiW/exec'
 const form = document.forms['submit-to-google-sheet']
-console.log(form)
+
 
 form.addEventListener('submit', e => {
     e.preventDefault()
